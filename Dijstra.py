5b3d6ab342ba7992f81eec6cdf9567029b7eaca0
Julius Armbrüster <julius.a.game@gmail.com>
2019-05-26 23:28:47 +0200

    Graph in own file

--- Dijstra.py
+++ Dijstra.py
@@ -1,37 +1,7 @@
 from BinaryHeap import BinaryHeap
-import json, time
+from Graph import Graph
 from functools import reduce
-
-class Graph:
-    def __init__(self):
-        self.edges = {}
-        self.nodes = []
-
-    def find(self, label):
-        return self.nodes.index(label)
-    def neighbors(self, node):
-        if node in self.edges:
-            return self.edges[node]
-
-        return []
-    
-    def from_json(self, path):
-        def reducer(acc, cur):
-            s = cur['source']
-            t = cur['target']
-            c = cur['cost']
-            if s not in acc:
-                acc[s] = []
-            acc[s].append((c, t))
-            if t not in acc:
-                acc[t] = []
-            acc[t].append((c,s))
-            return acc
-
-        f = open(path)
-        data = json.load(f)
-        self.edges = reduce(reducer, data["edges"], {})
-        self.nodes = list(map(lambda x: x["label"], data["nodes"]))
+import json, time
 
 def find(graph, start, goal):
     open_list = BinaryHeap()
@@ -67,13 +37,6 @@ def retrace_path(closed_list, start, goal, path=[]):
     return retrace_path(closed_list, start, prev, path)
 
 
-def timer(fun, args):
-    start = time.time()
-    rv = fun(*args)
-    end = time.time()
-    print(f"elapsed time {fun.__name__}: {end - start}")
-    return rv
-
 if __name__ == "__main__":
     graph = Graph()
     graph.from_json("graph.json")
@@ -81,7 +44,6 @@ if __name__ == "__main__":
     start = graph.find("Erde")
     end = graph.find("b3-r7-r4nd7")
 
-    #closed_list = timer(find, (graph, start, end))
     closed_list = find(graph, start, end)
     path = retrace_path(closed_list, start, end)
     total_dist = reduce(lambda a,c: a+c[0], path, 0)
---
+++ Graph.py
@@ -0,0 +1,31 @@
+class Graph:
+    def __init__(self):
+        self.edges = {}
+        self.nodes = []
+
+    def find(self, label):
+        return self.nodes.index(label)
+    def neighbors(self, node):
+        if node in self.edges:
+            return self.edges[node]
+
+        return []
+    
+    def from_json(self, path):
+        def reducer(acc, cur):
+            s = cur['source']
+            t = cur['target']
+            c = cur['cost']
+            if s not in acc:
+                acc[s] = []
+            acc[s].append((c, t))
+            if t not in acc:
+                acc[t] = []
+            acc[t].append((c,s))
+            return acc
+
+        f = open(path)
+        data = json.load(f)
+        self.edges = reduce(reducer, data["edges"], {})
+        self.nodes = list(map(lambda x: x["label"], data["nodes"]))
+
